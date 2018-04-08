8d22da1a62c19541eab4ec470ad2139e4dd4cec1
aj-delvalle <ajdelvalle@gmail.com>
2018-04-07 17:19:14 -0700

    add function cls() to clear grid

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,36 @@
-//function createDivs(){
-  var gridSize = 16;
-  var e = document.getElementsByClassName('main');
-  for(var i = 1; i <= gridSize * gridSize; i++){
-    var row = document.createElement('div');
-    row.className = 'row';
-    // for (var j = 1; j <= gridSize; j++) {
-    //   var cell = document.createElement('div');
-    //   cell.className = 'grid';
-    //   row.appendChild(cell);
-      row.addEventListener('mouseover',
-        (e) => {e.target.style.backgroundColor = 'black';
+var gridSize = 16;
+var gridModel = 'auto '.repeat(gridSize);
+const container = document.querySelector('.container');
+container.setAttribute('style', `grid-template-columns:${gridModel}; grid-template-rows:${gridModel};`)
+
+createGrid();
+
+function createGrid(){
+for(var i = 1; i <= gridSize; i++)
+{
+  for(var j = 0; j < gridSize; j++)
+    {
+	var grids = document.createElement('div');
+
+ 
+
+grids.setAttribute("class", "grids");
+
+
+
+  grids.addEventListener('mouseover',
+    (e) => {e.target.style.backgroundColor = 'black';
     });
 
-  e[0].appendChild(row);
-  }
+    container.appendChild(grids);
+    }
+}
+} //end of function createGrid
+function cls()
+{
+	while(container.firstChild){
+												container.removeChild(container.firstChild)
+	}
+	createGrid();
 
-
-//}
+};
